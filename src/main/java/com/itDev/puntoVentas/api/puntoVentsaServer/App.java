8c6ba15122ab5b74c38ed7eeae2df88bbaff14a9
cecylopez <ceciliac86548@gmail.com>
2018-01-15 00:44:04 -0600

    Creacion de proyecto punto de ventas
    Creacion de proyecto en maven

---
+++ src/main/java/com/itDev/puntoVentas/api/puntoVentsaServer/App.java
@@ -0,0 +1,13 @@
+package com.itDev.puntoVentas.api.puntoVentsaServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
