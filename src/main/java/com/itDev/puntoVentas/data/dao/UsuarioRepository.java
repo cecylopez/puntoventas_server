318f0b0dbdaaf0a0111c630ddd88fd0a372886a6
cecylopez <ceciliac86548@gmail.com>
2018-03-01 00:28:48 -0600

    Creacion UsuarioRepository
    Creacion de metodo para obtener un usuario y para iniciar sesion en el
    cual primero se obtiene el usuario y luego se valida la contrasena de lo
    contrario no se obtiene la informacion del usuario

--- src/main/java/com/itDev/puntoVentas/data/dao/UsuarioRepository.java
+++ src/main/java/com/itDev/puntoVentas/data/dao/UsuarioRepository.java
@@ -3,13 +3,24 @@ package com.itDev.puntoVentas.data.dao;
 import javax.persistence.TypedQuery;
 
 import com.itDev.puntoVentas.data.entities.Usuario;
+import com.itDev.puntoVentas.util.SecurityHelper;
 
-public class UsuarioRepository {
+public class UsuarioRepository extends BaseRepository<Usuario> {
 	
-	public Usuario getUsuario(String userName, String password) {
-		Usuario user= new Usuario();
-		//TypedQuery<Usuario>
-		return user;
+	public Usuario get(String nombre) {
+		Usuario usuario= null;
+		TypedQuery<Usuario> qry= eMgr.createQuery("SELECT u FROM Usuario u WHERE u.nombre = :nombre", Usuario.class);
+		qry.setParameter("nombre", nombre);
+		usuario= qry.getSingleResult();
+		return usuario;
+	}
+	
+	public Usuario login(String nombre, String password) {
+		Usuario usuario= get(nombre);
+		if(usuario != null && !SecurityHelper.verificar(password, usuario.getPassword())) {
+			usuario=null;
+		}
+		return usuario;
 	}
 
 }
